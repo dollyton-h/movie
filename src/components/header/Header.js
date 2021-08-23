ff56c019746bb2d44ece719108eab7d9d442574d
dollyton hutapea <dolly.hutapea@yahoo.com>
2021-08-23 11:00:58 +0700

    update auto complete

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,11 +3,29 @@ import "./header.scss";
 
 import { useDispatch } from "react-redux";
 import { searchAsync } from "../../redux/action/searchAction";
+import axios from "axios";
 
 function Header() {
   const [search, setSearch] = useState("");
   const [apiKey] = useState("f69f0628");
   const [more, setMore] = useState(1);
+  const [auto, setAuto] = useState([]);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const loadSearch = async () => {
+      const response = await axios.get(
+        `http://www.omdbapi.com/?s=${text}&apikey=${apiKey}&page=${more}`
+      );
+      console.log(response.data.Search);
+      setAuto(response.data.Search);
+    };
+    loadSearch();
+  }, [text]);
+
+  const onChangeHandler = (text) => {
+    setText(text);
+  };
 
   const dispatch = useDispatch();
   const searchForm = useRef(null);
@@ -15,6 +33,7 @@ function Header() {
   const handleClickEvent = () => {
     const form = searchForm.current;
     setSearch(`${form["search"].value}`);
+    setText("");
   };
 
   const moreHandle = () => {
@@ -29,7 +48,25 @@ function Header() {
     <div>
       <div className="header-main">
         <form ref={searchForm}>
-          <input name={"search"} />
+          <input
+            onChange={(e) => onChangeHandler(e.target.value)}
+            value={text}
+            name={"search"}
+          />
+          <div>
+            {auto &&
+              auto.map((e) => {
+                return (
+                  <div
+                    key={e.imdbID}
+                    onClick={() => setText(e.Title)}
+                    className="auto-c"
+                  >
+                    {e.Title}
+                  </div>
+                );
+              })}
+          </div>
         </form>
         <button onClick={handleClickEvent}>Search Movie</button>
         <button onClick={moreHandle}>More Movie...</button>
